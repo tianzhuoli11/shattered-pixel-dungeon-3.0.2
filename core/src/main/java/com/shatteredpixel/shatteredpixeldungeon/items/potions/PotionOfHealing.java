2849196060b1a86e643f0ce1fb7859735bda29fb
Sissie <linzhiping010702@126.com>
2026-05-13 19:05:37 +1000

    PotionOfHealing-- logging

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/potions/PotionOfHealing.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/potions/PotionOfHealing.java
@@ -39,6 +39,7 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
+import com.shatteredpixel.shatteredpixeldungeon.utils.PlayerEventLogger;
 
 public class PotionOfHealing extends Potion {
 
@@ -53,6 +54,9 @@ public class PotionOfHealing extends Potion {
 		identify();
 		cure( hero );
 		heal( hero );
+		int hpBefore = hero.HP;
+		PlayerEventLogger.info("PotionOfHealing", "POTION_DRINK",
+				"hero=" + hero.heroClass + ",hp=" + hpBefore + "->" + hero.HP + ",ht=" + hero.HT);
 	}
 
 	public static void heal( Char ch ){
